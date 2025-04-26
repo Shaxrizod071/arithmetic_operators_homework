e86f241078037bebc1ec5c36843c044527386a56
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-04-26 23:27:29 +0500

    dffg

--- arithmetic_op02.py
+++
@@ -1,7 +0,0 @@
-#Create a varaible 'number' and assign it the value of 43.
-a=43
-#Create a variable 'answer' and assign it the sum of the number's digits.
-b=a%10
-#Print the vallue of the 'answer'.
-c=a//10
-print(b+c)
