5ba5c11e4d30e83b5fee952385d6eb4a0213b9cf
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-04-26 17:18:03 +0500

    asdf

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,8 @@
-#Create a variable 'x' and assign it the value of 10.
-
+#Crate a variable 'x' and assign it the value of 10.
+a=10
 #Create a variable 'y' and assign it the value of 5.
-
+b=5
 #Create a variable 'result' and assign it the value of x + y.
-
-#Print the value of the 'result'.
+c=a+b
+#Print the value of the 'result
+print(c)
