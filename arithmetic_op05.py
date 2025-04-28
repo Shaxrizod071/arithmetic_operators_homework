7bd4f3211fb3ca147ea6b1727d1c611d45347436
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-04-28 12:24:12 +0500

    sas

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,7 @@
 #Create a variable called 'number' with data type int.
-
+a=2
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-
+v=3
 #Print 'answer'.
+b=a+v
+print(b)
